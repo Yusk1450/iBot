1afa83ad21a3c90d75b07995ca396cb8a92b3df3
Yusk <yusk1450@gmail.com>
2018-08-29 16:18:07 +0900

    Initial Commit

---
+++ iBotLib2/iBotLib2.h
@@ -0,0 +1,19 @@
+//
+//  iBotLib2.h
+//  iBotLib2
+//
+//  Created by Yusk1450 on 2018/08/29.
+//  Copyright © 2018年 Yusk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iBotLib2.
+FOUNDATION_EXPORT double iBotLib2VersionNumber;
+
+//! Project version string for iBotLib2.
+FOUNDATION_EXPORT const unsigned char iBotLib2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iBotLib2/PublicHeader.h>
+
+
